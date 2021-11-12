2070696583b5cd0920833c4d48df26f7121ba71d
Pierrick Turelier <pierrick@turelier.com>
2021-11-12 11:30:35 -0600

    fix(Constants): change network name to homestead

--- src/Constants.ts
+++ src/Constants.ts
@@ -5,12 +5,12 @@ const DAI_RINKEBY_ADDRESS = '0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa';
 const USDC_RINKEBY_ADDRESS = '0x4DBCdF9B62e891a7cec5A2568C3F4FAF9E8Abe2b';
 
 export const daiAddresses = {
-  mainnet: DAI_MAINNET_ADDRESS,
+  homestead: DAI_MAINNET_ADDRESS,
   rinkeby: DAI_RINKEBY_ADDRESS,
 } as { [key: string]: string };
 
 export const usdcAddresses = {
-  mainnet: USDC_MAINNET_ADDRESS,
+  homestead: USDC_MAINNET_ADDRESS,
   rinkeby: USDC_RINKEBY_ADDRESS,
 } as { [key: string]: string };
 
@@ -21,11 +21,11 @@ export const DAI_POD_RINKEBY_ADDRESS = '0x4A26b34A902045CFb573aCb681550ba30AA797
 export const USDC_POD_RINKEBY_ADDRESS = '0x68c96179Cf9a90C589571Dc7AA94AD15d94e917d';
 
 export const daiPodAddresses = {
-  mainnet: DAI_POD_MAINNET_ADDRESS,
+  homestead: DAI_POD_MAINNET_ADDRESS,
   rinkeby: DAI_POD_RINKEBY_ADDRESS,
 } as { [key: string]: string };
 
 export const usdcPodAddresses = {
-  mainnet: USDC_POD_MAINNET_ADDRESS,
+  homestead: USDC_POD_MAINNET_ADDRESS,
   rinkeby: USDC_POD_RINKEBY_ADDRESS,
 } as { [key: string]: string };
